58c36ce5db47d1ada668d63ab7bfd682b5a8384c
Jess Leyva <jessica.leyvat@yahoo.com>
2023-03-20 22:28:59 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(name){
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+
+prependCat= name => {let newCats = [...cats]; 
+newCats.unshift(name); return newCats; }
+
+removeLastCat = () =>{ let newCats = [...cats]; 
+newCats.pop(); return newCats;}
+
+removeFirstCat = () => {let newCats =[...cats]; 
+newCats.shift(); return newCats}
